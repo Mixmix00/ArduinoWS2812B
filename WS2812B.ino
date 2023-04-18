e67d8fd0161a9aabe8e0195ab3978da7abb844b7
TTVMixmix00 <68516760+TTVMixmix00@users.noreply.github.com>
2023-04-18 18:12:44 +0000

    added some rough code

--- WS2812B.ino
+++ WS2812B.ino
@@ -1,3 +1,14 @@
+#include <FastLED.h>
+
+#define LED_PIN 9
+#define NUM_LEDS 100
+
+CRGB leds[NUM_LEDS];
+
+
+  
+
+
 int colors[4][3] = {
 {200, 200, 200},
 {100, 100, 100},
@@ -11,14 +22,22 @@ int colors[4][3] = {
 
 void setup(){
     for(int i = 0; i < sizeof colors / sizeof colors[0]; i++){
-        //Serial.println();
+        
         for(int j = 0; j< sizeof colors[0] / sizeof(int); j++){
-            //Serial.print(colors[i][j]);
+            if(colors[i][j] > 256){
+                colors[i][j] = 256;
+            }else if(colors[i][j] < 0){
+                colors[i][j] = 0;
+            }
         }
     }
+
+    FastLED.addLeds<WS2812, LED_PIN, RGB>(leds, NUM_LEDS);
 }
 
 void loop(){
+
+    
     //If(button pressed on numpad){
         //Then change to color} else if(button pressed on numpad){
             //Then change to other color}...
@@ -26,7 +45,9 @@ void loop(){
 
 
     //If we wanted to add that you have to press and hold you could maybe add an else at the end or create a boolean and if one num on the numpad is pressed disable everything
-
+    // here, so
+    leds[0] = CRGB(colors[0][0],colors[0][1], colors[0][2]);
+    FastLED.show();
 
 
 }
