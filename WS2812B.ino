007c59ca6c38257e2fb49f60580e5552b55e0618
Max <mspier00001@gmail.com>
2023-04-18 18:54:38 -0700

    added some cool stuff. Better commit messages in the future

--- WS2812B.ino
+++ WS2812B.ino
@@ -1,4 +1,5 @@
 #include <FastLED.h>
+#include <Keypad.h>
 
 #define LED_PIN 9
 #define NUM_LEDS 100
@@ -6,22 +7,31 @@
 CRGB leds[NUM_LEDS];
 
 
-  
+  //Gets the individual numpad buttons
+char keypad[4][4] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
 
+short rowPins[4] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+short colPins[4] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
 
 int colors[4][3] = {
-{200, 200, 200},
-{100, 100, 100},
-{50, 50, 50},
-{50, 100, 200}
+    {200, 200, 200},
+    {100, 100, 100},
+    {50, 50, 50},
+    {50, 100, 200}
 } ;
 
-
+Keypad membraneKeypad;
 
 
 
 void setup(){
     Serial.begin(9600);
+    membraneKeypad = Keypad( makeKeymap(keypad), rowPins, colPins, 4, 4); //Creates the keypad
     //This is to make sure that the colors are not out of range
     for(int i = 0; i < sizeof colors / sizeof colors[0]; i++){
         for(int j = 0; j< sizeof colors[0] / sizeof(int); j++){
@@ -48,6 +58,8 @@ void loop(){
 
     //If we wanted to add that you have to press and hold you could maybe add an else at the end or create a boolean and if one num on the numpad is pressed disable everything
     // here, so
+
+    char customKey = membraneKeypad.getKey();
     
     leds[0] = CRGB(colors[0][0],colors[0][1], colors[0][2]);
     FastLED.show();
